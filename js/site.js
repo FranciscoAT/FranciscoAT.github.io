d9fe1f1814219e30abf0850aa45bd6f332d343a5
Francisco <francisco.ad.trindade@gmail.com>
2017-11-13 23:09:08 -0500

    refactored all the things

--- js/site.js
+++ js/site.js
@@ -9,4 +9,26 @@ $(document).ready(function() {
             scrollTop: $target.offset().top - 60
         }, 500);
     });
+
+    // Replace select SVG's with the inline SVG
+    // $('.project-card-arrow').each(function() {
+    //     console.log('hit');
+    //     var $img = $(this);
+    //     var imgClass = $img.attr('class');
+    //     var imgURL = $img.attr('url');
+
+    //     $.get(imgURL, function(data) {
+    //         var $svg = $(data).find('svg');
+
+    //         if (typeof imgClass !== 'undefine') {
+    //             $svg = $svg.attr('class', imgClass);
+    //         }
+
+    //         img.replaceWith($svg);
+    //     }, 'xml');
+    // });
+
+    //Hover for arrow SVG
+    // $('.project-card')
+    
 });
