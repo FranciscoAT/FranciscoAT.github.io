91a15745211b23a1cd65b8c32990654d3d70b120
Francisco <francisco.ad.trindade@gmail.com>
2017-11-25 13:42:41 -0500

    Quick commit to see if prefix fixes error on nav image

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,13 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var minify = require('gulp-clean-css');
+var prefixer = require('gulp-autoprefixer');
 
 gulp.task('compile-sass', function() {
     return gulp.src('css/*.scss')
         .pipe(sass())
         .pipe(concat('styles.min.css'))
+        .pipe(prefixer())
         .pipe(minify({compatibility:'*'}))
         .pipe(gulp.dest('css/'))
 });
